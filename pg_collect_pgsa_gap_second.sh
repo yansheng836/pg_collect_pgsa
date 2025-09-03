a873c4082d4dfb80fda099792ece461207ab6ccb
yansheng836 <sheng.yan836@gmail.com>
2025-09-03 15:57:46 +0800

    feat: update pg_collect_pgsa_gap_second.sh for add arg

--- pg_collect_pgsa_gap_second.sh
+++ pg_collect_pgsa_gap_second.sh
@@ -3,6 +3,29 @@
 # 设置脚本运行的最长时间（秒），这里设置为1分钟（60秒）
 MAX_RUNTIME=60
 
+# 设置默认睡眠时间为5秒
+DEFAULT_SLEEP=5
+# 检查是否有参数传入
+if [ $# -eq 0 ]; then
+    DEFAULT_SLEEP=5
+    #echo "未提供参数，使用默认睡眠时间: ${DEFAULT_SLEEP}秒"
+    #sleep $DEFAULT_SLEEP
+else
+    # 有参数
+    # 校验参数是否为 (0-60) 的整数
+    if ! [[ "$1" =~ ^[0-9]+$ ]]; then
+        echo "入参错误：参数 '$1' 不是整数。"
+        exit 1
+    fi
+
+    if [ "$1" -le 0 ] || [ "$1" -ge 60 ]; then
+        echo "入参错误：参数 '$1' 超出范围，请输入 (0-60) 之间的整数，不包括0和60。"
+        exit 1
+    else
+        DEFAULT_SLEEP=$1
+    fi
+fi
+
 # 记录脚本开始运行的时间（Unix时间戳）
 START_TIME=$(date +%s)
 
@@ -12,14 +35,13 @@ while true; do
     ELAPSED_TIME=$((CURRENT_TIME - START_TIME))
     
     if [ $ELAPSED_TIME -ge $MAX_RUNTIME ]; then
-        echo "脚本已运行超过 $MAX_RUNTIME 秒，即将退出..."
+        #echo "脚本已运行超过 $MAX_RUNTIME 秒，退出。"
         exit 0
     fi
 
-    sh /root/pg_collect_pgsa/pg_collect_pgsa.sh  # 替换为实际命令
-    sleep 5
-    #echo $1
-    #sleep $0
-    #exit 0
+    sh ./pg_collect_pgsa.sh  # 替换为实际命令
+
+    #sleep 5
+    sleep $DEFAULT_SLEEP
 done
 
